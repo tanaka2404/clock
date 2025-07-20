67de88ebd51956919edb06bcbc38bb63fe106c46
tanaka2404 <souta_ta0204@icloud.com>
2025-07-20 19:55:35 +0900

    add notation setting

--- js/clock.js
+++ js/clock.js
@@ -5,6 +5,17 @@ setInterval(() => {
     clock();
 }, 1000);
 
+let notationMode = document.getElementById('ClockNotation');//時計の表記設定
+let clockUpper = document.getElementById('main');//時計の上段
+let clockNotationMode = "all";
+notationMode.addEventListener('change', (e) => {
+    if(notationMode.value == "all"){
+        clockNotationMode = "all";
+    }else if(notationMode.value == "hmsOnly"){
+        clockNotationMode = "hmsOnly"
+    }
+});
+
 function clock(){
     let jikan = new Date();
     let clock = document.getElementById('main');
@@ -20,5 +31,9 @@ function clock(){
     if (hours <= 9  ){hours     = '0' + hours;    }
     if (minutes <= 9){minutes   = '0' + minutes   }
     if (seconds <= 9){seconds   = '0' + seconds;  }
-    clock.innerHTML = `<h1>${year}:${month}:${date}</h1>` + "<br>" + `<h1>${hours}:${minutes}:${seconds}</h1>`;
+    if (clockNotationMode == "all"){
+        clock.innerHTML = `<h1>${year}:${month}:${date}</h1>` + "<br>" + `<h1>${hours}:${minutes}:${seconds}</h1>`;
+    }else {
+    clock.innerHTML = `<h1>${hours}:${minutes}:${seconds}</h1>`;
+    }
 }
--- js/main/setting.js
+++ js/main/setting.js
@@ -1,23 +1,32 @@
-const cookies = document.cookie;
-const array = cookies.split(';');
-
-array.forEach(function(value) {
-    const content = value.split('=');
-    console.log(content[1]); // valueを取得
-})
+function light(){
+    document.cookie = 'Theme=light';
+    document.documentElement.style.setProperty('--default-black', 'black');
+    document.documentElement.style.setProperty('--default-white', 'white');
+}
+function dark(){
+    document.cookie = 'Theme=dark';
+    document.documentElement.style.setProperty('--default-black', 'white');
+    document.documentElement.style.setProperty('--default-white', 'black');
+}
 let designMode = document.getElementById('designMode');
 designMode.addEventListener('change', (e) => {
     if(designMode.value == "light"){
-        document.cookie = "Theme=light; max-age=0";
-        document.cookie = 'Theme=light';
-        document.documentElement.style.setProperty('--default-black', 'black');
-        document.documentElement.style.setProperty('--default-white', 'white');
-        console.log("light");
+        light();
     }else if(designMode.value == "dark"){
-        document.cookie = "Theme=dark; max-age=0";
-        document.cookie = 'Theme=dark';
-        document.documentElement.style.setProperty('--default-black', 'white');
-        document.documentElement.style.setProperty('--default-white', 'black');
-        console.log("dark");
+        dark();
     }
 });
+
+window.onload = function(){
+    const cookies = document.cookie;
+    const array = cookies.split(';');
+    let result = array.indexOf('Theme=light');
+    let subResult = array.indexOf('Theme=dark');
+    if ( result > -1){
+        designMode.value = "light";
+        light();
+    }else if ( subResult > -1){
+        designMode.value = "dark";
+        dark();
+    }
+}
